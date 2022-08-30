a86bd04b958d3ab7a7e683e4e61ed41dcedcecc3
anaramosiv <anakrs18@gmail.com>
2022-08-30 21:47:23 +0200

    entrega final

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -4,7 +4,7 @@ const useLocalStorage = (key, initial) => {
 	const [value, setValue] = useState(() => {
 		return JSON.parse(window.localStorage.getItem(key)) || initial;
 	});
-
+	
 	const setNewValue = (newValue) => {
 		setValue(newValue);
 		window.localStorage.setItem(key, JSON.stringify(newValue));
